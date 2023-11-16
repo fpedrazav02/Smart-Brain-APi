60bb9a193757d76c85d80840530f85c46a77afde
Francisco Pedraza vazquez <fpedraza@MacBook-Pro-de-Francisco.local>
2023-11-16 23:59:10 +0100

    API improvement -> fe connections

--- server.js
+++ server.js
@@ -1,4 +1,91 @@
-let i = 0;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 3000;
+
+const tempdataBase = {
+  users: [
+    {
+      id: 1,
+      name: "Pepe",
+      email: "pepe@dummy.com",
+      password: "dummy1",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: 2,
+      name: "Ramon",
+      email: "ramon@dummy.com",
+      password: "dummy2",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: 3,
+      name: "Juan",
+      email: "juan@dummy.com",
+      password: "dummy3",
+      entries: 0,
+      joined: new Date(),
+    },
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.send(tempdataBase.users);
+})
+
+/*
+ * API ENDPOINTS PLAN
+ *      /register | post -> success/fail
+ *      /signing  | post -> success/
+ *
+ * */
+
+app.post('/signing', (req, res) => {
+  res.send('Correcto');
+})
+
+app.post('/register', (req, res) => {
+  if ((req.body.email === null) || (req.body.password = null)) {
+    res.send("Lo siento. Ha habido un error");
+  }
+
+  const { email, name, password } = req.body;
+
+  const user = {
+    id: 3,
+    name: `${name}`,
+    email: `${email}`,
+    password: `${password}`,
+    entries: 0,
+    joined: new Date(),
+  }
+  tempdataBase.users.push(user);
+
+  res.json(user);
+})
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+
+  tempdataBase.users.forEach((user) => {
+    if (user.id == id) {
+      return res.json(user);
+    }
+  })
+  res.status(404).send('No such user');
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
 
 
